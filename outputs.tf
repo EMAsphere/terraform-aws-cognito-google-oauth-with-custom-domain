7d249bf79a60b85e38236c87b4f2eb0770a2e7dd
Thibault Gérondal <tgerondal@emasphere.com>
2023-09-07 21:50:32 +0200

    Output json formatted for alb annotation auth-idp-cognito
    Signed-off-by: Thibault Gérondal <tgerondal@emasphere.com>

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,13 @@ output "pool_id" {
 output "cloudfront_distribution_arn" {
   value = aws_cognito_user_pool_domain.custom.cloudfront_distribution_arn
 }
+
+output "alb_json" {
+  value = jsonencode({
+    "alb.ingress.kubernetes.io/auth-idp-cognito" = {
+      userPoolARN      = aws_cognito_user_pool.users.arn,
+      userPoolClientID = aws_cognito_user_pool_client.cognito.id,
+      userPoolDomain   = local.custom_domain
+    }
+  })
+}
